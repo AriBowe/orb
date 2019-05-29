7348bc7638e4af0e81deb9b80b568b0e033ed953
Ari Bowe <ari.otis.bowe@gmail.com>
2019-05-29 21:52:32 +1000

    Basic design for orbs

--- cogs/orb_economy.py
+++ cogs/orb_economy.py
@@ -3,24 +3,43 @@ from discord.ext import commands as bot_commands
 import csv
 import random
 
-class User(bot_commands.Cog):
+class User():
     """An orb user, this class manages their economy"""
     def __init__(self):
         pass
 
-    async def get_orbs(self):
+    async def get_orbs(self, user_id):
+        with open("data/orbs.csv", mode="r", newline="") as file:
+                reader = csv.reader(file, delimiter=",")
+                for line in reader:
+                    try:
+                        print(line[0])
+                        if line[0] == user_id:
+                            return line[2]
+                    except:
+                        pass
+
+    async def add_orbs(self, user_id, orb_type, orb_count):
         pass
 
-    async def add_orbs(self, orb_type, orb_count):
+    async def remove_orbs(self, user_id, orb_type, orb_count):
         pass
 
-    async def remove_orbs(self, orb_type, orb_count):
+    async def get_shards(self, user_id):
         pass
 
+    async def add_shards(self, user_id, shard_count):
+        pass
 
-class Orb(bot_commands.Cog):
+    async def remove_shards(self, user_id, shard_count):
+        pass
+
+    async def save(self):
+        pass
+
+class Orb():
     """Representation of an orb, the basic unit of an item.
-    Should not be initialsed on it's own"""
+    Should not be initialised on its own"""
     def __init__(self):
         pass
     
