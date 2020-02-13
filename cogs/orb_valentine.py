fe7729f4bbb26b38867c982c181eeca12a35ad4a
Ari Bowe <ari.otis.bowe@gmail.com>
2020-02-13 23:24:39 +1000

    updated to only use roled members

--- cogs/orb_valentine.py
+++ cogs/orb_valentine.py
@@ -16,15 +16,20 @@ from utils import default, repo
 from cogs.orb_control import allowed_channel, db
 
 
+
+
 class Valentine(bot_commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self._active_users = []
         self._last_active_check = datetime(2001, 9, 13)
+        
+        self.SMACK_GUILD = bot.get_guild(286411114969956352)
+        self.SMACK_ID = 286411114969956352
 
     @bot_commands.command(aliases=["loves", "love", "valentine", "crush"])
     async def valentines(self, ctx, *, target=None):
-        if ctx.guild.id != 286411114969956352:
+        if ctx.guild.id != self.SMACK_ID:
             await ctx.send("Sorry, but my cherub powers only extend to SMACK UQ!")
             return
 
@@ -74,8 +79,15 @@ class Valentine(bot_commands.Cog):
         """
 
         if self._last_active_check < datetime.utcnow() + timedelta(minutes=5):
+            active_users = self.SMACK_GUILD.get_role(371079720768634895)        # Execs
+            active_users.append(self.SMACK_GUILD.get_role(592939685723242496))  # Mods
+            active_users.append(self.SMACK_GUILD.get_role(286412638508941312))  # Senpai
+            active_users.append(self.SMACK_GUILD.get_role(286411614855626752))  # Old Exec
+            active_users.append(self.SMACK_GUILD.get_role(585601647149842465))  # Boost
+            active_users.append(self.SMACK_GUILD.get_role(607914165121843230))  # Member
+
             self._last_active_check = datetime.utcnow()
-            self._active_users = self.bot.get_guild(286411114969956352).members            
+            self._active_users = active_users            
         
         return random.choice(self._active_users).id
 
