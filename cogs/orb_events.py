ffb9951066a955d9ee5490ef708a3529684d1e38
ari.otis.bowe@gmail.com <ari.otis.bowe@gmail.com>
2020-02-27 13:04:55 +1000

    can't spell

--- cogs/orb_events.py
+++ cogs/orb_events.py
@@ -211,7 +211,7 @@ class Events(bot_commands.Cog):
         elif re.search(r"\b(:fox:)\b", message.content, re.IGNORECASE):
             await message.add_reaction("🦊")
 
-        elif re.search(r"\b(surely)\b", message,content, re.IGNORECASE):
+        elif re.search(r"\b(surely)\b", message.content, re.IGNORECASE):
             for emote in "🇸🇺🇷🇪🇱🇾":
                 await message.add_reaction(emote)
 
