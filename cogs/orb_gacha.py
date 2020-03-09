5d546693ea8ea16049b8d4921e00f4cf0b27e236
ari.otis.bowe@gmail.com <ari.otis.bowe@gmail.com>
2020-03-10 08:40:24 +1000

    gacha

--- cogs/orb_gacha.py
+++ cogs/orb_gacha.py
@@ -1,1 +1,22 @@
-##
+"""God is dead"""
+
+# Standard imports
+import discord
+import random
+
+from discord.ext import commands as bot_commands
+
+# Local bot imports
+from utils import default, repo
+from cogs.orb_control import allowed_channel, db
+
+class Gacha(bot_commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @bot_commands.command
+    async def gacha(self, ctx):
+        await ctx.send(random.choice(["Double!","Nothing!"]))
+
+def setup(bot):
+    bot.add_cog(Gacha(bot))
