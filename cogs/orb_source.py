95c9d9fcffedf9a8a29ca169ab63ff91541dc7a0
Ari Bowe <ari.otis.bowe@gmail.com>
2021-01-21 17:40:31 +1000

    added support for an actual API key rather than doing it via IP

--- cogs/orb_source.py
+++ cogs/orb_source.py
@@ -9,7 +9,6 @@ from utils import repo, logger
 class SauceCog(bot_commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.source = SauceNao()
 
     @bot_commands.command(aliases=["source"])
     async def sauce(self, ctx):
@@ -17,19 +16,21 @@ class SauceCog(bot_commands.Cog):
             await ctx.send("This command only functions when used in a reply!")
             return
         
+        source = SauceNao(api_key=repo.config["keys"]["saucenao_api_key"])
+
         reference = await ctx.channel.fetch_message(ctx.message.reference.message_id)
         if len(reference.attachments) > 0 or len(reference.embeds):
             output = ""
             for attachment in reference.attachments:
                 try:
-                    output += f"\n{self.source.from_url(attachment.url)[0].urls[0]} (similarity: {self.source.from_url(attachment.url)[0].similarity})"
-                except:
-                    logger.log("Source", f"Failed to read attachment {attachment.url}")
+                    output += f"\n{source.from_url(attachment.url)[0].urls[0]} (similarity: {source.from_url(attachment.url)[0].similarity})"
+                except Exception as e:
+                    logger.log("Source", f"Failed to read attachment {attachment.url}. {e}")
             for embed in reference.embeds:
                 try:
-                    output += f"\n{self.source.from_url(embed.thumbnail.url)[0].urls[0]} (similarity: {self.source.from_url(embed.thumbnail.url)[0].similarity})"
-                except:
-                    logger.log("Source", f"Failed to read embed {embed.thumbnail.url}")
+                    output += f"\n{source.from_url(embed.thumbnail.url)[0].urls[0]} (similarity: {source.from_url(embed.thumbnail.url)[0].similarity})"
+                except Exception as e:
+                    logger.log("Source", f"Failed to read embed {embed.thumbnail.url}. {e}")
             if len(output) > 0:
                 await ctx.send(f"Here's my best guess at the sources: {output}")
             else:
