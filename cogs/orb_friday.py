64ef0b2de23c28e4b80679a498876effcc39e952
Paul Clarke <paulioclarke@icloud.com>
2021-04-02 18:04:57 +1000

    Tested and added the SMACK general chat ID to the command. This should work, but will need to be confirmed in-person at 9 AM in #general

--- cogs/orb_friday.py
+++ cogs/orb_friday.py
@@ -1,13 +1,8 @@
 import discord
 from datetime import datetime, timedelta
-#import datetime
-#import schedule
 import asyncio
 from discord.ext import commands as bot_commands
 from discord.ext import tasks
-
-#bot = bot_commands.Bot(command_prefix="o")
-client = discord.Client()
 
 class FridayCog(bot_commands.Cog):
     def __init__(self, bot):
@@ -19,12 +14,11 @@ class FridayCog(bot_commands.Cog):
 
     @tasks.loop(minutes=1.0)
     async def announcement(self):
-        channel = self.bot.get_channel(823075283942375458)
-
+        channel = self.bot.get_channel(286411114969956352)
         current = datetime.now()
         if (current.strftime("%a") == "Fri" and current.strftime("%H") == "09" and current.strftime("%M") == "00"):
-            await ctx.send(file=discord.File(fp="images/happyFriday.mp4"))
-            await ctx.send(":tuturu: SMACK! Happy Friday!!")
+            await channel.send(file=discord.File(fp="images/happyFriday.mp4"))
+            await channel.send(":sumakotuturu: SMACK! Happy Friday!")
             
 def setup(bot):
     bot.add_cog(FridayCog(bot))
